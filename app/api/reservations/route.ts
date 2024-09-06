bdc09a9d44a16a8fd16399d6cd6c83409034fbb4
thembinkosi <twala.emma@gmail.com>
2024-09-06 10:03:36 +0200

    deleted containers

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -8,15 +8,19 @@ export const POST = async (req: Request) => {
     const { userId, containerId, timeslotId } = await req.json();
 
     const booking = await db.booking.create({
-      data: {
-        userId,
-        containerId,
-        timeslotId,
-      },
-    });
-
+    data: {
+      userId,
+      user: { connect: { id: userId } }, // Connect to the user
+      containerId,
+      container: { connect: { id: containerId } }, // Connect to the container
+      timeslotId,
+      timeslot: { connect: { id: timeslotId } }, // Connect to the timeslot
+      referenceNumber: '', // Add this
+    },
+  });
     return NextResponse.json(booking);
   } catch (error) {
     return new NextResponse("Something went wrong!", { status: 500 });
   }
 };
+ 
