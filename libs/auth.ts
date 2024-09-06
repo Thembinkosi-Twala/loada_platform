fa0c5cf38fe85142fc361c7d7bc6e614bf259fe9
thembinkosi <twala.emma@gmail.com>
2024-09-06 10:43:53 +0200

    deleted containers

--- libs/auth.ts
+++ libs/auth.ts
@@ -1,66 +1,61 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import NextAuth, { AuthOptions } from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from "bcrypt"
-
-import prisma from "./db";
+import bcrypt from "bcrypt";
+import { AuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import  db  from "./db";
 
 export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(db),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     CredentialsProvider({
-      name: "credentials" as string,
+      name: "credentials",
       credentials: {
         email: { label: "email", type: "text" },
-        password: { label: "password", type: "password" },
+        password: {
+          label: "password",
+          type: "password",
+        },
       },
+
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error("Invalid credentials")
+          throw new Error("Invalid credentials");
         }
 
-        const user = await prisma.user.findUnique({
+        const user = await db.user.findUnique({
           where: {
             email: credentials.email,
           },
-        })
+        });
 
-        if (!user || !user?.hashedPassword) {
-          throw new Error("Invalid credentials")
-        }
+        if (!user || !user?.password) throw new Error("Invalid credentials");
 
-        const isValid = await bcrypt.compare(
+        const isCorrectPassword = await bcrypt.compare(
           credentials.password,
-          user.hashedPassword
-        )
+          user.password
+        );
 
-        if (!isValid) {
-          throw new Error("Invalid Password")
-        }
+        if (!isCorrectPassword) throw new Error("Invalid credentials");
 
-        return user
+        return user;
       },
     }),
   ],
   pages: {
     signIn: "/",
   },
-
-  debug: process.env.NODE_ENV === "development",
   session: {
     strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
-}
-
-export default NextAuth(authOptions)
+};
