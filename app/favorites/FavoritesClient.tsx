d97c83f316f279f804d64364108be13cbe6260eb
thembinkosi <twala.emma@gmail.com>
2024-09-06 10:07:18 +0200

    deleted containers

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 import ListingCard from '@/components/listings/ListingCard'
 import Heading from '@/components/Heading'
 import Container from '@/components/Container'
-import { User, Listing } from '@prisma/client'
+import { User, Booking } from '@prisma/client'
 
 interface FavoritesClientProps {
-  listings: Listing[],
-  currentUser?: User | null,
+  listings: Booking[];
+  currentUser?: User | null;
 }
 
 
@@ -21,13 +21,7 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({ listings, currentUser
       <div
         className="mt-6 md:mt-10 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 md:gap-8 gap-4"
       >
-        {listings.map((listing) => (
-          <ListingCard
-            currentUser={currentUser}
-            key={listing.id}
-            data={listing}
-          />
-        ))}
+    
       </div>
     </Container>
   )
