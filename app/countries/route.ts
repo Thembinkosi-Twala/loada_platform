5f07ba8735c01895672fbad3df61e80d89996c98
TwalaT-Code <twala.emma@gmail.com>
2024-11-16 15:31:46 +0200

    edited routes

--- app/countries/route.ts
+++ app/countries/route.ts
@@ -1,21 +1,16 @@
-// app/countries/route.ts
-
 import { NextResponse } from "next/server";
-import  db  from "@/libs/db";
+import db from "@/libs/db";
 
-interface IParams {
-  containerNumber?: string;
-  size?: string;
-}
-
-export const GET = async (req: Request, { params }: { params: IParams }) => {
+export const GET = async (req: Request) => {
   try {
-    const { containerNumber, size } = params;
+    const url = new URL(req.url);
+    const containerNumber = url.searchParams.get("containerNumber") || undefined;
+    const size = url.searchParams.get("size") || undefined;
 
     const cargos = await db.container.findMany({
       where: {
-        containerNumber: containerNumber || undefined,
-        size: size || undefined,
+        containerNumber,
+        size,
       },
     });
 
